1e93c99802031c68fb5ae6e3744b92e72929ccea
Sigurdur E. Thorvaldsson <sigurdur@bu.edu>
2016-10-06 17:08:37 -0400

    Added tester files

---
+++ w5_testpoly.py
@@ -0,0 +1,9 @@
+# AUTHOR test jbc@bu.edu
+# AUTHOR jbc jbc@bu.edu
+# AUTHOR brower brower@bu.edu
+import unittest
+
+authors=['test@bu.edu','jbc@bu.edu','brower@bu.edu']
+
+class PolynomialTestCase(unittest.TestCase):
+    "empty class"
