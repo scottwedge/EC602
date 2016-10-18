8bfddd04b2341ea8c6b08ea5ca008ac95828bf40
John <jfkeis@bu.edu>
2016-10-18 17:31:33 -0400

    w6_test_poly.py fixed test_fft

--- w6_dft_test.py
+++ w6_dft_test.py
@@ -3,43 +3,43 @@
 # AUTHOR John Keisling jfkeis@bu.edu
 # AUTHOR Sigurdur Egill Thorvaldsson sigurdur@bu.edu
 
-w6_dft.py
+w6_dft_test.py
 10/17/16
 Siggi&John
 """
 
-import DFT
+#import DFT
 import unittest
 import numpy as np
-import random
-import sys
+
+
+authors = ['jfkeis@bu.edu','sigurdur@bu.edu']
 
 class DFTTestCase(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+
     def test_type(self):
         z = DFT((1, 4, 1+1j, 3-2j, 4-6j))
-        self.assertIsInstance(z,np.array,"Not a sequence of numbers")
+        self.assertIsInstance(z,np.ndarray)
         
     def test_shape(self):
         y = np.array([1, 4, 1+1j, 3-2j, 4-6j])
         z = DFT((1, 4, 1+1j, 3-2j, 4-6j))
         self.assertEqual(z.shape, y.shape)
-    
+
     def test_fft(self):
-        for i in range(0, 10):
-            x1 = (random.random()*2)-1
-            i1 = (random.random()*2)-1
-            i1 = complex(0,i1)
-            x2 = (random.random()*2)-1
-            i2 = (random.random()*2)-1
-            i2 = complex(0,i2)
-            x3 = (random.random()*2)-1
-            i3 = (random.random()*2)-1
-            i3 = complex(0,i3)
-            x4 = (random.random()*2)-1
-            i4 = (random.random()*2)-1
-            i4 = complex(0,i4)
-            x = (x1+i1, x2+i2, x3+i3, x4+i4)
-            DFTfft = DFT(x)
-            z = np.asarray(x, dtype=complex)
-            numpfft = np.fft.fft(z)
-            self.assertAlmostEqual(numpfft,DFTfft)
+        for N in range(2,21):
+            x = []
+            for i in range(0, 10):
+                for j in range(0,N+1):
+                    x1 = (np.random.random()*2)-1
+                    i1 = (np.random.random()*2)-1
+                    n1 = complex(x1,i1)
+                    x = x.append(n1)
+                DFTfft = DFT(x)
+                z = np.asarray(x, dtype=complex)
+                numpfft = np.fft.fft(z)
+                for k in range(0,N):
+                    self.assertAlmostEqual(numpfft[k],DFTfft[k])
