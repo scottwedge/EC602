99f47d4840e8f662240a0f9e12fa2a029d1081a3
John <jfkeis@bu.edu>
2016-11-04 16:39:53 -0400

    added w8_multiply python program

---
+++ w8_multiply.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+# AUTHOR John Keisling jfkeis@bu.edu
+# AUTHOR Sigurdur Egill Thorvaldsson sigurdur@bu.edu
+
+w8_multiply
+11/7/16
+Siggi&John
+"""
+
+# WAV file support
+import scipy.io.wavfile as wavfile
+#import PyQt4.QtGui as qt
+import time
+import numpy as np
+#import matplotlib.pyplot as pyplot
+#import math
