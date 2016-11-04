3639f41e95dd4088af8e773b3b86b5bf6b9bc326
John <jfkeis@bu.edu>
2016-11-04 18:00:51 -0400

    w8_multiply python functional

--- w8_multiply.py
+++ w8_multiply.py
@@ -8,10 +8,24 @@ w8_multiply
 Siggi&John
 """
 
-# WAV file support
-import scipy.io.wavfile as wavfile
-#import PyQt4.QtGui as qt
-import time
 import numpy as np
 #import matplotlib.pyplot as pyplot
 #import math
+
+f = open('xtest10.txt','r')
+A=[]
+for line in f.readlines():
+    A.append(line.split())
+f.close
+
+f = open('xtest11.txt','r')
+B=[]
+for line in f.readlines():
+    B.append(line.split())
+f.close
+
+a=np.array(A).astype('int')
+b=np.array(B).astype('int')
+
+C = np.dot(a,b)
+print(C)
