813f0cea993bc89c52be561608e7a3687054579f
Sigurdur E. Thorvaldsson <sigurdur@bu.edu>
2016-11-06 11:23:25 -0500

    can't remember what I did

---
+++ w8/py_open_test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+# AUTHOR John Keisling jfkeis@bu.edu
+# AUTHOR Sigurdur Egill Thorvaldsson sigurdur@bu.edu
+w8_multiply
+11/7/16
+Siggi&John
+"""
+
+import numpy as np
+import sys
+
+f = open('testfile', 'r+')
+print(f)
+
+f.readline()
+list(f)
--- w8/w8c_multiply.cpp
+++ w8/w8c_multiply.cpp
@@ -5,26 +5,52 @@
 #include <string>
 
 using namespace std;
+
+// typedef vector< vector<int> > int_matrix;
+
+// int_matrix multply(const & int_matrix A,const & int_matrix B){
+
+//  int M = A.size();
+//  int K = A[0].size();
+//  int L = B[0].size();
+
+//  int_matrix c(M,L);
+ 
+//  for (int i=0;i<M;i++)
+//     for (int j=0;j<L;j++)
+//         for (int n=0;n<N;n++)
+//             c[i][j] = A[i][n]*B[n][j]
+
+
+// }
+
+
 int main(int argc, char const *argv[])
 {
-
-	ifstream thisfile; // note: this is an IFSTREAM, "I" stands for INPUT
+	ifstream thisfile1, thisfile2; // note: this is an IFSTREAM, "I" stands for INPUT
 
 	int one,two,three,four;
 	vector<int> squares;       
     
+	thisfile1.open(argv[1]);
+	thisfile2.open(argv[2]);
 
-	thisfile.open("file1.txt");
-
-	while (thisfile >> one >> two >> three >> four)
+	while (thisfile1 >> one >> two >> three >> four)
 	{ 
 		cout << one << " " << two << " " << three << " " << four << endl;
 		//squares.push_back(two);
     }
+	
+	while (thisfile2 >> one >> two >> three >> four)
+	{ 
+		cout << one << " " << two << " " << three << " " << four << endl;
+		//squares.push_back(two);
+    }    
 
-    thisfile.close(); 
+    thisfile1.close();
+    thisfile2.close();
 
-    cout << argv[1];
+    cout << thisfile1[1] << endl;
 
 	// ofstream filewithmatrix3;
 
