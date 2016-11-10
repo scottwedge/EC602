5927cc21fa6089a97db7de490afa9fa197e30097
Sigurdur E. Thorvaldsson <sigurdur@bu.edu>
2016-11-10 17:00:26 -0500

    moved file from w8 to w9

---
+++ w9_10/w9_wordplayer.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+# AUTHOR John Keisling jfkeis@bu.edu
+# AUTHOR Sigurdur Egill Thorvaldsson sigurdur@bu.edu
+
+w8_tester
+11/7/16
+Siggi&John
+"""
+
+import sys
+import numpy
+
+bw = numpy.loadtxt(big_wordlist)
+for i in bw:
+	print(i)
