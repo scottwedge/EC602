63ee4d6d9944b1af76178fe1cadcca49a37e81a2
John <jfkeis@bu.edu>
2016-11-13 23:08:56 -0500

    works in command line. passes his checker, reads back all of the neatness errors. needs to be fixed to print out each word on separate line

--- w9_wordplayer.py
+++ w9_wordplayer.py
@@ -15,32 +15,11 @@ import sys
 #innum = "8"
 ext = False;
 
-"""
-def search(wrd, num):
-    while i<=len(wrd):
-      it=itertools.permutations(wrd,i)
-      for y in it:
-        a.append("".join(y))
-      i+=1 
-    a.sort(key=len,reverse=True)
-    possible_words=[]
-    for x in a:
-      if uk.check(x) and x not in possible_words :
-        possible_words.append(x) 
-    
-    possible_words.sort(key=len,reverse=True)
-    for x in possible_words:
-      print(x)
-"""
-
 #with open('big_wordlist.txt', "r") as word_list:
 #    words = list(word_list)
 
 with open(sys.argv[1], "r") as word_list:
     words = list(word_list)
-
-#def get_input():
-#    return raw_input('enter the word: ').rstrip('\r\n')
 
 def compress_wordlist(biglist, l):
     return [biglist.strip() for biglist in biglist if len(biglist) == l+1]
@@ -63,19 +42,22 @@ def test(word, n):
 
 if __name__ == '__main__':
     while(ext == False):
-        #user_input = input()
-        #wrd = user_input.split()
-        print(wrd)
-        #inword = user_input[0]
-        #n = int(user_input[1])
-        print(inword)
-        print(n)
-        # from timeit import Timer
-        inword = 'berdache'
-        n = 8;
+        user_input = input()
+        wrd = user_input.split()
+        #print(wrd)
+        inword = wrd[0]
+        n = int(wrd[1])  # could not convert string to int: 'e'
+        #print(inword)
+        #print(n)
+        #from timeit import Timer
+        #inword = 'berdache'
+        #n = 8;
         res = test(inword, n)
-        #res = [''.join(p) for p in itertools.permutations('berdache', n-1)]
-        print(res)
-        ext = True
-        # t = Timer("test(word)", "from __main__ import test, word")
-        # print t.timeit(number=500)
+        res = list(res)
+        #for i in res:        
+        #    print(res[i])
+        #ext = True
+        if(n == 0):
+            ext = True
+        #t = Timer()
+        #print(t.timeit(number=500))
