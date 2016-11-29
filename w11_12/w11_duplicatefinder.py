0c55bf893384a928a34e0873de7380391c52fa5f
Sigurdur E. Thorvaldsson <sigurdur@bu.edu>
2016-11-29 17:29:38 -0500

    made folder for w11/12

---
+++ w11_12/w11_duplicatefinder.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+# AUTHOR John Keisling jfkeis@bu.edu
+# AUTHOR Sigurdur Egill Thorvaldsson sigurdur@bu.edu
+
+w8_tester
+11/7/16
+Siggi&John
+"""
+from os import listdir
+import re
+from skimage.io import imread
+import numpy as np
+import hashlib
